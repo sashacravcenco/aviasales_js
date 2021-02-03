168f6d413b9df7dff2312523acc7d4a7f9d58c0f
splincode <omaxphp@yandex.com>
2021-02-03 16:15:58 +0200

    connected cities api

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ const inputCitiesTo = document.querySelector('.input__cities-to');
 const dropdownCitiesTo = document.querySelector('.dropdown__cities-to');
 const inputDateDepart = document.querySelector('.input__date-depart');
 
-const city = ['Москва', 'Санкт-Петербург', 'Минск', 'Караганда', 'Челябинск', 'Керчь', 'Волгоград', 'Самара',
-    'Днепропетровск', 'Екатеринбург', 'Одесса', 'Нижний Новгород', 'Калининград', 'Кишинев'
-];
+const citiesApi = 'http://api.travelpayouts.com/data/ru/cities.json';
+const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+let city = [];
+
+
+const getData = (url, callback) => {
+    fetch(url)
+        .then((response) => response.json())
+        .then(callback);
+}
 
 const showCity = (input, list) => {
     list.textContent = '';
@@ -15,20 +23,16 @@ const showCity = (input, list) => {
     if (input.value === '') return;
 
     const filterCity = city.filter((item) => {
-        const fixItem = item.toLowerCase();
+        const fixItem = item.name.toLowerCase();
         return fixItem.includes(input.value.toLowerCase());
     });
     filterCity.forEach((item) => {
         const li = document.createElement('li');
         li.classList.add('dropdown__city');
-        li.textContent = item;
+        li.textContent = item.name;
         list.append(li);
     });
 };
-
-inputCitiesFrom.addEventListener('input', () => {
-    showCity(inputCitiesFrom, dropdownCitiesFrom)
-});
 
 function handleCityClick(target, inputCities, dropdownCities) {
     if (target.tagName.toLowerCase() === 'li') {
@@ -37,14 +41,22 @@ function handleCityClick(target, inputCities, dropdownCities) {
     }
 }
 
-dropdownCitiesFrom.addEventListener('click', (event) => {
-    handleCityClick(event.target, inputCitiesFrom, dropdownCitiesFrom);
+inputCitiesFrom.addEventListener('input', () => {
+    showCity(inputCitiesFrom, dropdownCitiesFrom);
 });
 
 inputCitiesTo.addEventListener('input', () => {
     showCity(inputCitiesTo, dropdownCitiesTo)
 });
 
+dropdownCitiesFrom.addEventListener('click', (event) => {
+    handleCityClick(event.target, inputCitiesFrom, dropdownCitiesFrom);
+});
+
 dropdownCitiesTo.addEventListener('click', (event) => {
     handleCityClick(event.target, inputCitiesTo, dropdownCitiesTo);
+});
+
+getData(proxy + citiesApi, (data) => {
+    city = data;
 });
